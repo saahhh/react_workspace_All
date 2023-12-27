321c8eff4855bbd09c70866d28e8aac97540d1a9
saahhh <143372306+saahhh@users.noreply.github.com>
2023-12-27 17:32:38 +0900

    .

--- Final-react-website/client/src/App.js
+++ Final-react-website/client/src/App.js
@@ -1,6 +1,85 @@
 //AppMain.js
 //App.js 둘 다 내용 동일함
 
+// App.js
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+function App() {
+  const [cafes, setCafes] = useState([]);
+  const [newCafe, setNewCafe] = useState({ name: " ", price: " " });
+
+  useEffect(() => {
+    const timestamp = new Date().getTime();
+    axios
+      .get(`http://localhost:5003/api/cafe?timestamp=${timestamp}`)
+      .then((response) => {
+        console.log("Cafes Data:", response.data);
+        setCafes(response.data);
+      })
+      .catch((error) => console.error("에러입니다.", error));
+  }, []);
+
+  const addCafe = () => {
+    axios
+      .post("http://localhost:5003/api/cafe", newCafe)
+      .then(() => {
+        const timestamp = new Date().getTime();
+        axios
+          .get(`http://localhost:5003/api/cafe?timestamp=${timestamp}`)
+          .then((response) => {
+            console.log("Cafes Data after addition:", response.data);
+            setCafes(response.data);
+            setNewCafe({ name: " ", price: " " });
+          })
+          .catch((error) => console.error("에러입니다.", error));
+      })
+      .catch((error) => console.error("에러입니다.", error));
+  };
+
+  return (
+    <div>
+      <h1>카페 리스트</h1>
+
+      <ul>
+        {cafes.map((cafe) => (
+          <li key={cafe.ID}>
+            {cafe.NAME} - {cafe.PRICE}원
+          </li>
+        ))}
+      </ul>
+
+      <h2>새로운 메뉴 추가</h2>
+      <div>
+        <label>Name:</label>
+        <input
+          type="text"
+          value={newCafe.name}
+          onChange={(e) => setNewCafe({ ...newCafe, name: e.target.value })}
+        />
+      </div>
+      <div>
+        <label>Price:</label>
+        <input
+          type="text"
+          value={newCafe.price}
+          onChange={(e) => setNewCafe({ ...newCafe, price: e.target.value })}
+        />
+      </div>
+      <button onClick={addCafe}>추가하기</button>
+    </div>
+  );
+}
+
+export default App;
+
+/*
+key={cafe[0] = key={cafe.ID}
+        cafe[1] = cafe.NAME
+        cafe[2] = cafe.PRICE
+*/
+
+/*
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 
@@ -24,6 +103,7 @@ function AppCafe() {
       .then((response) => setCafes(response.data))
       .catch((error) => console.error("에러입니다", error));
   });
+
   return (
     <div>
       <h1>카페 메뉴</h1>
@@ -56,83 +136,4 @@ function AppCafe() {
 }
 
 export default AppCafe;
-
-/*
-key={cafe[0] = key={cafe.ID}
-        cafe[1] = cafe.NAME
-        cafe[2] = cafe.PRICE
-*/
-
-/*
-// App.js
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-
-function App() {
-  const [cafes, setCafes] = useState([]);
-  const [newCafe, setNewCafe] = useState({ name: "", price: "" });
-
-  useEffect(() => {
-    const timestamp = new Date().getTime();
-    axios
-      .get(`http://localhost:5003/api/cafes?timestamp=${timestamp}`)
-      .then((response) => {
-        console.log("Cafes Data:", response.data);
-        setCafes(response.data);
-      })
-      .catch((error) => console.error("에러입니다.", error));
-  }, []);
-
-  const addCafe = () => {
-    axios
-      .post("http://localhost:5003/api/cafes", newCafe)
-      .then(() => {
-        const timestamp = new Date().getTime();
-        axios
-          .get(`http://localhost:5003/api/cafes?timestamp=${timestamp}`)
-          .then((response) => {
-            console.log("Cafes Data after addition:", response.data);
-            setCafes(response.data);
-            setNewCafe({ name: "", price: "" });
-          })
-          .catch((error) => console.error("에러입니다.", error));
-      })
-      .catch((error) => console.error("에러입니다.", error));
-  };
-
-  return (
-    <div>
-      <h1>카페 리스트</h1>
-
-      <ul>
-        {cafes.map((cafe) => (
-          <li key={cafe[0]}>
-            {cafe[1]} - {cafe[2]}원
-          </li>
-        ))}
-      </ul>
-
-      <h2>새로운 메뉴 추가</h2>
-      <div>
-        <label>Name:</label>
-        <input
-          type="text"
-          value={newCafe.name}
-          onChange={(e) => setNewCafe({ ...newCafe, name: e.target.value })}
-        />
-      </div>
-      <div>
-        <label>Price:</label>
-        <input
-          type="text"
-          value={newCafe.price}
-          onChange={(e) => setNewCafe({ ...newCafe, price: e.target.value })}
-        />
-      </div>
-      <button onClick={addCafe}>추가하기</button>
-    </div>
-  );
-}
-
-export default App;
 */
--- Final-react-website/server/server.js
+++ Final-react-website/server/server.js
@@ -7,7 +7,7 @@ const PORT = 5003;
 
 //들어오는 url은 허용을 안해준 상태
 //Get과 Post로 DB에 전달받을 url주소도 허용할 수 있도록 넣어줘야함
-app.use(cors({ origin: "http://localhost:3001" }));
+app.use(cors({ origin: "http://localhost:3002" }));
 
 //get으로 가져가는 주소만 허용해준 것
 //app.use(cors()); => 이 코드는 껐다가 다시 실행해줘야함
@@ -29,7 +29,7 @@ async function selectQuery(sql) {
     return result.rows.map((row) => ({
       ID: row[0],
       NAME: row[1],
-      PRICE: [2],
+      PRICE: row[2],
     }));
   } catch (err) {
     console.error(err);
@@ -55,7 +55,7 @@ app.get("/api/cafe", async (request, response) => {
 });
 
 //post로 전달받을 쿼리 작성해주기
-app.post("/api/cafes", async (request, response) => {
+app.post("/api/cafe", async (request, response) => {
   const { name, price } = request.body;
   console.log("데이터 들어왔는지 확인! : ", { name, price });
 
@@ -63,13 +63,13 @@ app.post("/api/cafes", async (request, response) => {
   try {
     connection = await oracledb.getConnection(dbconfig);
     await connection.execute(
-      "INSERT INTO cafe (ID, NAME, PRICE) VALUES (CAFE_SEQ.NEXTVAL, :name :price)",
+      "INSERT INTO cafe (ID, NAME, PRICE) VALUES (CAFE_SEQ.NEXTVAL, :name, :price)",
       { name, price },
       { autoCommit: true }
     );
     response.json({ message: "성공적으로 저장되었습니다." });
   } catch (error) {
-    console.error("Error in POST /api/cafes : ", error);
+    console.error("Error in POST /api/cafe : ", error);
   } finally {
     if (connection) {
       try {
